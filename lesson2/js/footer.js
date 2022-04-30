6ea2039d9d4e62b3be47ad1dbfa260e722cb19f9
AdonisAeron <tjones869@byui.edu>
2022-04-30 01:49:53 -0400

    Added JS to get year and last modify.

---
+++ lesson2/js/footer.js
@@ -0,0 +1,6 @@
+let d = new Date()
+let mDate = document.lastModified
+
+nFooter = `© ${d.getFullYear()} | Trevor C. Jones | Last Modified: ${mDate}`
+
+document.querySelector(".sFooter").textContent = nFooter
