b7e539c86699b8d6a9a85ec275cf110521a6fb29
AdonisAeron <tjones869@byui.edu>
2022-05-21 14:29:00 -0400

    Finished out the website with JS

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -2,6 +2,8 @@ let d = new Date();
 let mDate = document.lastModified;
 const datefield = document.querySelector(".date");
 const fulldate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(d);
+const myHeader = document.getElementById("myHeader")
+cDay = d.getDay()
 
 nfooter = `© ${d.getFullYear()} Spartanburg Chamber | Trevor C. Jones | WDD 230 Project | Last Modified : ${mDate}`;
 
@@ -10,6 +12,14 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
 };
+
+if(cDay == 1 || cDay == 2) {
+
+    var tag = document.createElement("p");
+    var Msg = document.createTextNode("🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.");
+    tag.appendChild(Msg)
+    myHeader.appendChild(tag)
+}
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
 document.getElementById("hamburgerBtn").addEventListener("click", toggleMenu);
