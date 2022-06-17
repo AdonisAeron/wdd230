5d3e375fe9b5c0e3734be712f78ad67cbeb8ddd5
AdonisAeron <tjones869@byui.edu>
2022-06-17 00:35:17 -0400

    Minor Updates

--- chamber/js/buisness.js
+++ chamber/js/buisness.js
@@ -1,4 +1,4 @@
-const requestURL = "../data.json";
+const requestURL = "https://adonisaeron.github.io/wdd230/chamber/data.json";
 const busi = document.querySelector(".buisnesses");
 
 fetch(requestURL)
@@ -6,5 +6,38 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject)
-    })
+        const business = jsonObject['Businesses'];
+        business.forEach(displayBusiness);
+    });
+
+function displayBusiness(business) {
+    let card = document.createElement("section");
+    let img = document.createElement("img");
+    let name = document.createElement("h1");
+    let pNum = document.createElement("p");
+    let Addr = document.createElement("p");
+    let website = document.createElement("a");
+
+    img.setAttribute('src', business.imageurl);
+    img.setAttribute('alt', `${business.name} Logo`);
+    img.setAttribute('loading', 'lazy');
+
+    name.textContent = business.name;
+    Addr.textContent = business.address;
+    pNum.textContent = business.pNumber;
+    website.textContent = business.website;
+
+    if(business.website == "N/A") {
+        business.website = "https://www.mcdonalds.com/us/en-us.html";
+    };
+
+    website.setAttribute('href', business.website);
+
+    card.appendChild(img);
+    card.appendChild(name);
+    card.appendChild(Addr);
+    card.appendChild(pNum);
+    card.appendChild(website);
+
+    document.querySelector("div.buisnesses").appendChild(card);
+};
