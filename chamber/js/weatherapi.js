a051249729ab14283a04387bcb55814a86796f59
AdonisAeron <tjones869@byui.edu>
2022-06-23 15:38:43 -0400

    finalized JS

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -1,6 +1,9 @@
 const currentTemp = document.querySelector(".Temp");
 const weatherIcon = document.querySelector(".wImage");
 const captionDesc = document.querySelector('.clouds');
+const windSpeed = document.querySelector(".wSpeed");
+const clTemp = document.querySelector(".Temp");
+const windChill = document.querySelector(".wChill");
 const requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=Spartanburg&units=imperial&appid=ec9e25a5242ce0422f6c28f2f6bbd926"
 
 async function apiFetch() {
@@ -24,15 +27,26 @@ function displayResults(weatherData) {
 
     const iconSrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
+    const wSpeed = weatherData.wind.speed;
+
     let words = desc.split(" ");
     
     for(let i = 0; i < words.length; i++) {
         words[i] = words[i][0].toUpperCase() + words[i].substr(1);
     }
 
+    if(clTemp <= 50 && wSpeed > 3) {
+
+        let wChill = (35.74 + (0.6245 * clTemp) - (35.75 * (wSpeed ** 0.16)) + (0.4275 * clTemp * (wSpeed ** 0.16)))
+        let fwChill = Math.round((wChill + Number.EPSILON) * 100) / 100;
+
+        windChill.innerHTML = `<strong>${fwChill} &#8457;</strong>`;
+    }
+
     weatherIcon.setAttribute('src', iconSrc);
     weatherIcon.setAttribute('alt', words);
     captionDesc.textContent = words.join(" ");
+    windSpeed.textContent = wSpeed;
 }
 
 apiFetch();
