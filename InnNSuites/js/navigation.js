79a2cd4da79572b80024a014d0483578526e94b9
AdonisAeron <tjones869@byui.edu>
2022-07-19 12:35:13 -0400

    Added Last Modified statement

--- InnNSuites/js/navigation.js
+++ InnNSuites/js/navigation.js
@@ -1,7 +1,13 @@
+let d = new Date();
+let mDate = document.lastModified;
+
+nfooter = `© ${d.getFullYear()} Temple Inn & Suites | Trevor C. Jones | WDD 230 Project | Last Modified : ${mDate}`;
+
 function toggleMenu() {
     console.log("Working");
     document.getElementById("primaryNav").classList.toggle('open');
     document.getElementById("hamburgerBtn").classList.toggle('open');
 }
 
+document.querySelector(".lModify").textContent = nfooter
 document.getElementById("hamburgerBtn").addEventListener('click', toggleMenu);
